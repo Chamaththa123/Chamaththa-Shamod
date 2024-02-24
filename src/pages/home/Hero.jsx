5fb5c019a4795d1e6d285f6c17f57c77808832bf
Chamaththa123 <shamodchamaththa@gmail.com>
2024-02-25 01:25:30 +0530

    hero

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useSpring, animated, easings } from "react-spring";
 import { useInView } from "react-intersection-observer";
-import hero from './../../assets/images/hero.webp'
-import hero2 from './../../assets/images/logo.webp'
+import hero from './../../assets/images/Frame65.png'
+import hero2 from './../../assets/images/user.png'
 // import hero from './../../assets/images/hero.jpeg'
 
 import { Link } from 'react-router-dom';
@@ -71,25 +71,41 @@ export const Hero = ({ scrollRefs }) => {
 };
   return (
     <section className='overflow-hidden w-full xl:h-screen h-[80vh] relative flex items-center justify-center  font-press-start' ref={ref}>
-      <div className=' hero-overlay bg-gradient-to-t from-[#004be06e] to-[#004be015]'></div>
-      <img src={hero} className=' w-full xl:h-[100vh] h-[80vh] object-cover' alt="" />
-      <div className=' absolute  w-full xl:h-screen h-[80vh] md:px-[6%] flex items-left flex-col justify-center'>
-      <animated.div style={fadeTitle} className="text-white font-raleway xl:text-[70px] text-left text-5xl xl:leading-[100px] leading-[20px]  font-semibold  ">
-      <span className="md:text-3xl "> Hi, </span>
-          <span className="md:text-3xl"> I am </span>
-        </animated.div>
-        <animated.div style={fadeTitle} className="text-white font-raleway xl:text-[65px]  text-5xl xl:leading-[100px] leading-[60px]  font-semibold  ">
-        Chamaththa
-          Shamod
-        </animated.div>
-        <animated.div style={fadeDes} className=" text-white font-raleway xl:text-3xl text-xl font-medium leading-[5px]">
-        Passionate Software Engineer | <br></br>Transforming Ideas into Code
+    <div className='absolute top-0 left-0 w-full z-50 bg-[#0c2051]'></div>
+    {/* <div className=' hero-overlay'></div> */}
+    <img src={hero} className=' w-full xl:h-screen h-[80vh] object-cover' alt="" />
+    <div className=' absolute  w-full xl:h-screen h-[80vh] md:px-[6%] flex flex-col justify-center'>
+      <div className="flex md:flex-row flex-col">
+        <div className="md:w-[60%]">
+          <animated.div style={fadeTitle} className="text-white font-raleway xl:text-[70px] text-left text-5xl xl:leading-[100px] leading-[20px] font-semibold">
+            <span className="md:text-3xl">Hi,</span>
+            <span className="md:text-3xl">I am</span>
           </animated.div>
-          
-        <animated.button style={fadeDes}  onClick={() => handleNavigateToSection(scrollRefs.contact)} to="/" className='px-[25px] py-[15px] font-raleway md:px-[20px] md:py-[10px] rounded-[10px] border-2 border-[#F14902] hover:bg-[#bd6742b6]  text-white xl:text-lg text-base font-semibold md:mt-20 mt-10 w-[15%]'>
-          My Resume
-        </animated.button>
+          <animated.div style={fadeTitle} className="text-white font-raleway xl:text-[65px] text-5xl xl:leading-[100px] leading-[60px] font-semibold">
+            Chamaththa
+            Shamod
+          </animated.div>
+          <animated.div style={fadeDes} className="text-white font-raleway xl:text-2xl text-xl font-medium leading-[5px]">
+            Passionate Software Engineer | Transforming Ideas into Code
+          </animated.div>
+          <animated.div style={fadeDes} className="text-white font-raleway xl:text-lg text-xl font-medium md:my-[7%] leading-[5px] md:w-[80%]">
+            Welcome to my portfolio showcasing a journey fueled by passion, dedication, and a relentless drive for learning and growth.
+          </animated.div>
+          <div className='flex gap-[20px] md:mt-[5%]'>
+            <Link to="/contact" className='px-[20px] py-[10px] rounded-[10px] border-2 border-orange-500 hover:bg-orange-500 text-white xl:text-xl text-base font-semibold'>
+              Download CV
+            </Link>
+          </div>
+        </div>
+        <div className="md:w-[40%] flex justify-end">
+  <animated.div className="rounded-[10px] w-[90%] h-auto overflow-hidden" style={fadeRight}>
+    <img src={hero2} className="w-[100%] xl:h-auto object-cover" alt="" />
+  </animated.div>
+</div>
+
+
       </div>
-    </section>
+    </div>
+  </section>
   )
 }
