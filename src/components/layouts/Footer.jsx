bae1448b4da5aa8fd20379932e0ccb147a85267e
Chamaththa123 <shamodchamaththa@gmail.com>
2024-04-07 11:01:33 +0530

    footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSpring, animated, easings } from "react-spring";
 import { useInView } from "react-intersection-observer";
 
-import logo from './../../assets/images/logo.webp'
+import logo from './../../assets/images/logo2.png'
 import { FaInstagramSquare, FaFacebook, FaLinkedin } from "react-icons/fa";
 import { footerServices, headerItems } from '../../utils/dataArrays';
 import { Link, useNavigate } from "react-router-dom";
@@ -61,29 +61,18 @@ export const Footer = ({ scrollRefs}) => {
 
   return (
     <animated.section style={fadeScreen} className='overflow-hidden flex flex-col w-full h-auto' ref={ref}>
-      <div className='flex items-start flex-col md:flex-row justify-between  gap-10 md:px-[5%] p-[40px] bg-black '>
+      <div className='flex items-start flex-col md:flex-row justify-between  gap-10 md:px-[5%] p-[40px] ' style={{backgroundImage: 'linear-gradient(to left bottom, #00adef,  #3871c1)'}}>
         <div className=' flex flex-col items-center md:items-start md:w-[30%]'>
-          <img className="w-52 h-auto" src={logo} />
+          <img className="w-[30%] h-auto pb-10" src={logo} />
           <div className='flex flex-col gap-7 items-center md:items-start'>
             <div className="md:w-[70%] text-white text-base font-normal leading-normal text-center md:text-start">
-              Elevating Automotive Excellence and Building Lasting Relationships – Your Trusted Partner in Every Drive.
+            Passionate Software Engineer | Transforming Ideas into Code
             </div>
-            <div className="border-t-[1px] border-[#F9F9F9] w-[100%] md:w-[70%] hidden md:block  " />
-            <div className=' md:flex justify-between w-[100%] md:w-[40%] hidden'>
-              <span className=' w-[30px] cursor-pointer'>
-                <FaLinkedin className=' text-[30px] text-white' />
-              </span>
-              <span className=' w-[30px] cursor-pointer'>
-                <FaInstagramSquare className=' text-[30px] text-white' />
-              </span>
-              <span className=' w-[30px] cursor-pointer'>
-                <FaFacebook className=' text-[30px] text-white' />
-              </span>
-            </div>
+            {/* <div className="border-t-[1px] border-[#F9F9F9] w-[100%] md:w-[70%] hidden md:block  " /> */}
           </div>
 
         </div>
-        <div className='flex flex-col md:flex-row md:items-start items-center gap-10 w-full md:w-[30%] justify-between'>
+        <div className='flex flex-col md:flex-row md:items-start items-center gap-10 w-full md:w-[30%] md:px-[7%] justify-between'>
           <div className=' flex flex-col gap-7 '>
             <div className="text-center md:text-left text-white text-opacity-50 text-xl  uppercase leading-[50px]">Links</div>
 
@@ -94,7 +83,7 @@ export const Footer = ({ scrollRefs}) => {
               <div onClick={() => handleNavigateToSection(scrollRefs.contact)} className="text-white text-base font-medium cursor-pointer ">Contact Us</div>
             </div>
           </div>
-          <div className='flex flex-col gap-7 '>
+          {/* <div className='flex flex-col gap-7 '>
             <div className="text-center md:text-left text-white text-opacity-50 text-xl  uppercase leading-[50px]">Services</div>
 
             <div className='flex flex-col items-center md:items-start gap-6'>
@@ -106,31 +95,42 @@ export const Footer = ({ scrollRefs}) => {
                 )
               })}
             </div>
-          </div>
+          </div> */}
         </div>
 
-        <div className='flex flex-col md:items-start items-center gap-4 md:gap-5 w-full md:w-[20%] justify-between'>
+        <div className='flex flex-col md:items-start items-center gap-4 md:gap-5 w-full md:w-[30%] md:px-[7%] justify-between'>
           <div className="text-center md:text-left text-white text-opacity-50 text-xl  uppercase leading-[50px]">Contact</div>
 
           <div className='flex tracking-[0.2px] text-base text-white'>
-            0435 908 921
+           070-3826675
           </div>
 
           <div className='flex tracking-[0.2px] text-base text-white'>
-            Stautobase829@gmail.com
+            shamodchamththa@gmail.com
           </div>
-          <div className='flex tracking-[0.1px] text-base text-white'>
+          {/* <div className='flex tracking-[0.1px] text-base text-white'>
             1/36 Major Street, <br /> Pinelands NT 0829.
-          </div>
+          </div> */}
+          <div className=' md:flex justify-between w-[100%] md:w-[40%] hidden md:gap-7'>
+              <span className=' w-[30px] cursor-pointer'>
+                <FaLinkedin className=' text-[30px] text-white' />
+              </span>
+              <span className=' w-[30px] cursor-pointer'>
+                <FaInstagramSquare className=' text-[30px] text-white' />
+              </span>
+              <span className=' w-[30px] cursor-pointer'>
+                <FaFacebook className=' text-[30px] text-white' />
+              </span>
+            </div>
         </div>
       </div>
-      <div className="flex w-full bg-black md:text-base md:leading-[20px] justify-center ">
+      <div className="flex w-full bg-[#3062a8] md:text-base md:leading-[20px] justify-center md:py-[0.8%]">
         <div className='text-white text-base leading-[23px] flex flex-col md:flex-row gap-1 items-center'>
-          <span>© 2023 ST - Autobase. All Rights Reserved.</span>
+          <span>© 2024 - All Rights Reserved.</span>
           <span className="hidden md:block">|</span>
 
           <Link target="_blank" to="https://asipiya.lk//">
-            An Asipiya Soft website
+            Chamaththa Shamod
           </Link>
         </div>
       </div>
--- src/pages/home/AboutUs.jsx
+++ src/pages/home/AboutUs.jsx
@@ -1,114 +1,135 @@
 import React, { useEffect, useState } from "react";
 import { useSpring, animated, easings } from "react-spring";
 import { useInView } from "react-intersection-observer";
-import about from './../../assets/images/about-me.jpg'
-import circle from './../../assets/images/circle.svg'
+import about from "./../../assets/images/about-me.jpg";
+import circle from "./../../assets/images/circle.svg";
 export const AboutUs = ({ scrollref }) => {
-	const [isVisible, setIsVisible] = useState(false);
-	const [ref, inView] = useInView({
-		triggerOnce: true,
-	});
+  const [isVisible, setIsVisible] = useState(false);
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
 
-	const prevDelay = 200;
+  const prevDelay = 200;
 
-	const fadeScreen = useSpring({
-		opacity: isVisible ? 1 : 0,
+  const fadeScreen = useSpring({
+    opacity: isVisible ? 1 : 0,
 
-		config: {
-			duration: 800,
-		},
-	});
+    config: {
+      duration: 800,
+    },
+  });
 
-	const fadeSubTitle = useSpring({
-		opacity: isVisible ? 1 : 0,
-		transform: isVisible ? "translateY(0)" : "translateY(25px)",
-		config: {
-			duration: 800,
-			delay: prevDelay,
-			easing: easings.easeInSine,
-		},
-	});
-	const fadeTitle = useSpring({
-		opacity: isVisible ? 1 : 0,
-		transform: isVisible ? "translateY(0)" : "translateY(25px)",
-		config: {
-			duration: 800,
-			delay: prevDelay + 100,
-			easing: easings.easeInSine,
-		},
-	});
-	const fadeDes = useSpring({
-		opacity: isVisible ? 1 : 0,
-		transform: isVisible ? "translateY(0)" : "translateY(25px)",
-		config: {
-			duration: 800,
-			delay: prevDelay + 200,
-			easing: easings.easeInSine,
-		},
-	});
+  const fadeSubTitle = useSpring({
+    opacity: isVisible ? 1 : 0,
+    transform: isVisible ? "translateY(0)" : "translateY(25px)",
+    config: {
+      duration: 800,
+      delay: prevDelay,
+      easing: easings.easeInSine,
+    },
+  });
+  const fadeTitle = useSpring({
+    opacity: isVisible ? 1 : 0,
+    transform: isVisible ? "translateY(0)" : "translateY(25px)",
+    config: {
+      duration: 800,
+      delay: prevDelay + 100,
+      easing: easings.easeInSine,
+    },
+  });
+  const fadeDes = useSpring({
+    opacity: isVisible ? 1 : 0,
+    transform: isVisible ? "translateY(0)" : "translateY(25px)",
+    config: {
+      duration: 800,
+      delay: prevDelay + 200,
+      easing: easings.easeInSine,
+    },
+  });
 
-	const fadeLeft = useSpring({
-		opacity: isVisible ? 1 : 0,
-		transform: isVisible ? "translateX(0)" : "translateX(20%)",
-		config: {
-			duration: 800,
-			delay: prevDelay,
-			easing: easings.easeInSine,
-		},
-	});
+  const fadeLeft = useSpring({
+    opacity: isVisible ? 1 : 0,
+    transform: isVisible ? "translateX(0)" : "translateX(20%)",
+    config: {
+      duration: 800,
+      delay: prevDelay,
+      easing: easings.easeInSine,
+    },
+  });
 
-	useEffect(() => {
-		const loaderDelay = 50;
+  useEffect(() => {
+    const loaderDelay = 50;
 
-		// Simulate loading delay with setTimeout
-		setTimeout(() => {
-			if (inView) {
-				setIsVisible(true);
-			}
-		}, loaderDelay);
-	}, [inView]);
-	return (
-		<animated.section style={fadeScreen} ref={ref} className='overflow-hidden flex p-[30px] py-[60px] xl:px-[5%] xl:py-[4%] xl:flex-row flex-col gap-[5%] items-center w-full h-auto'>
-<div className='relative hidden xl:flex xl:w-[50%] h-[100%]  flex-col gap-5  items-center justify-center '>
-				<animated.div style={fadeLeft} className=' rounded-[10px] max-w-[520px] xl:max-h-[680px]  overflow-hidden'>
-					<div className=' absolute top-[-100px] left-[-110px] -z-10'>
-
-						<img
-							src={circle}
-							className="w-[100%] "
-							alt=""
-						/>
-					</div>
-					<img
-						src={about}
-						className="w-[100%]  xl:h-auto h-[356px] object-cover z-50"
-						alt=""
-					/>
-				</animated.div>
-			</div>
-			<div className=' xl:w-[50%] flex flex-col gap-7 xl:gap-1 items-start'>
-				<div ref={scrollref} />
-				<animated.div className="text-center text-[#40b93c] text-4xl font-medium xl:leading-[60px]" style={fadeSubTitle}>
-					About Me
-				</animated.div>
-				{/* <animated.div className="text-black xl:text-5xl text-3xl font-bold xl:leading-[70px] leading-[50px]" style={fadeTitle}>
+    // Simulate loading delay with setTimeout
+    setTimeout(() => {
+      if (inView) {
+        setIsVisible(true);
+      }
+    }, loaderDelay);
+  }, [inView]);
+  return (
+    <animated.section
+      style={fadeScreen}
+      ref={ref}
+      className="overflow-hidden flex p-[30px] py-[60px] xl:px-[5%] xl:py-[4%] xl:flex-row flex-col gap-[5%] items-center w-full h-auto"
+    >
+      <div className="relative hidden xl:flex xl:w-[50%] h-[100%]  flex-col gap-5  items-center justify-center ">
+        <animated.div
+          style={fadeLeft}
+          className=" rounded-[10px] max-w-[520px] xl:max-h-[680px]  overflow-hidden"
+        >
+          <div className=" absolute top-[-100px] left-[-110px] -z-10">
+            <img src={circle} className="w-[100%] " alt="" />
+          </div>
+          <img
+            src={about}
+            className="w-[100%]  xl:h-auto h-[356px] object-cover z-50"
+            alt=""
+          />
+        </animated.div>
+      </div>
+      <div className=" xl:w-[50%] flex flex-col gap-7 xl:gap-1 items-start">
+        <div ref={scrollref} />
+        <animated.div
+          className="text-center text-[#40b93c] text-4xl font-medium xl:leading-[60px]"
+          style={fadeSubTitle}
+        >
+          About Me
+        </animated.div>
+        {/* <animated.div className="text-black xl:text-5xl text-3xl font-bold xl:leading-[70px] leading-[50px]" style={fadeTitle}>
 					Driven by Excellence, Defined by Service
 				</animated.div> */}
-				<animated.div className=' xl:hidden rounded-[10px] w-full h-auto  overflow-hidden' style={fadeSubTitle}>
-					<img
-						src={about}
-						className="w-[100%]  xl:h-auto object-cover "
-						alt=""
-					/>
-				</animated.div>
+        <animated.div
+          className=" xl:hidden rounded-[10px] w-full h-auto  overflow-hidden"
+          style={fadeSubTitle}
+        >
+          <img
+            src={about}
+            className="w-[100%]  xl:h-auto object-cover "
+            alt=""
+          />
+        </animated.div>
 
-				<animated.div style={fadeDes} className=" text-black text-lg font-normal leading-[30px] xl:leading-10 flex flex-col gap-5">
-					<p> a passionate and driven software engineering intern with a keen interest in crafting innovative solutions to real-world problems. Throughout my academic journey and hands-on experiences, I've honed my skills in [list specific languages, tools, and technologies you're proficient in]. During my internships and personal projects, I've had the opportunity to delve into diverse areas of software development, including [mention any relevant areas such as web development, mobile app development, database management, etc.]. My approach to problem-solving involves a blend of creativity, analytical thinking, and attention to detail, ensuring that the solutions I develop are not only robust but also user-friendly.
-					</p>
-				</animated.div>
-
-			</div>
-			
-		</animated.section>
-	)
-}
+        <animated.div
+          style={fadeDes}
+          className=" text-black text-lg font-normal leading-[30px] xl:leading-10 flex flex-col gap-5"
+        >
+          <p>
+            {" "}
+            a passionate and driven software engineering intern with a keen
+            interest in crafting innovative solutions to real-world problems.
+            Throughout my academic journey and hands-on experiences, I've honed
+            my skills in [list specific languages, tools, and technologies
+            you're proficient in]. During my internships and personal projects,
+            I've had the opportunity to delve into diverse areas of software
+            development, including [mention any relevant areas such as web
+            development, mobile app development, database management, etc.]. My
+            approach to problem-solving involves a blend of creativity,
+            analytical thinking, and attention to detail, ensuring that the
+            solutions I develop are not only robust but also user-friendly.
+          </p>
+        </animated.div>
+      </div>
+    </animated.section>
+  );
+};
